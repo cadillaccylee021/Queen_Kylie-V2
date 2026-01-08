0a28c81a26a08bd3dbe04e5e809bb90be38b3adb
cadillaccylee021 <musachauke021@gmail.com>
2026-01-08 10:30:05 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
-  author: '‎',
+  packname: 'KynexorTechnologies',
+  author: '‎Queen_Kylie-V2',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botOwner: 'KynexorTechnologies', // Your name
+  ownerNumber: '27751014718', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
