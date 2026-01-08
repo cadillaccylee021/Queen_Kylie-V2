7687968e223a24cdaffd99a826d411ec11764b23
cadillaccylee021 <musachauke021@gmail.com>
2026-01-08 10:47:17 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const settings = {
   packname: 'KynexorTechnologies',
   author: '‎Queen_Kylie-V2',
-  botName: "Knight Bot",
+  botName: "QUEEN_KYLIE-V2 ❤️🧸",
   botOwner: 'KynexorTechnologies', // Your name
   ownerNumber: '27751014718', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
@@ -9,7 +9,7 @@ const settings = {
   maxStoreMessages: 20, 
   storeWriteInterval: 10000,
   description: "This is a bot for managing group commands and automating tasks.",
-  version: "3.0.5",
+  version: "2.0.0",
   updateZipUrl: "https://github.com/mruniquehacker/Knightbot-MD/archive/refs/heads/main.zip",
 };
 
